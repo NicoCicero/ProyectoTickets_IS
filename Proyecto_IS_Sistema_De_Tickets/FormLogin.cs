77f3887a758a03763563288243fd24eabebe2687
Cicero, Nicolas Ezequiel <NicolasEzequiel.Cicero@alumnos.uai.edu.ar>
2025-11-02 00:32:40 -0300

    Multi idioma terminado

--- Proyecto_IS_Sistema_De_Tickets/FormLogin.cs
+++ Proyecto_IS_Sistema_De_Tickets/FormLogin.cs
@@ -38,6 +38,7 @@ namespace Proyecto_IS_Sistema_De_Tickets
         {
             string sql = AdminSeedHelper.BuildUpdateAdminSql("admin@sistema.com", "Admin123!");
             Console.WriteLine(sql);
+
         }
 
         private void txt_Usuario_Enter(object sender, EventArgs e)
